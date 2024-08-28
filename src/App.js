fdb0c8e45ff1a577e1c2a2d6cc2f1998056402de
Kayathri-maths <kayualagarsamy@gmail.com>
2024-08-28 19:08:29 +0530

    Managing Multiple States

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   const expenses = [
     {
@@ -34,6 +35,7 @@ function App() {
   return (
     <div>
       <h1>Let's get Started</h1>
+      <NewExpense />
       <Expenses expenses={expenses}></Expenses>
     </div>
   );
