dfdfe92cb8b4ee914dbc698163f34a78d7a0bbd2
Kayathri-maths <kayualagarsamy@gmail.com>
2024-08-28 18:19:09 +0530

    useState Hook

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,18 +1,4 @@
 import "./ExpenseDate.css";
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
 
 function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
